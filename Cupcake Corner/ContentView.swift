aa7f5a1b58e850b81de27b46969635c08fe2d459
Sebastien REMY <seb.remy@me.com>
2020-06-18 10:09:27 +0200

    Checking for a valid address

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -8,44 +8,7 @@
 
 import SwiftUI
 
-class Order: ObservableObject {
-    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
-
-    @Published var type = 0
-    @Published var quantity = 3
-
-    @Published var specialRequestEnabled = false {
-        didSet {
-            if specialRequestEnabled == false {
-                extraFrosting = false
-                addSprinkles = false
-            }
-        }
-    }
-    
-    @Published var extraFrosting = false
-    @Published var addSprinkles = false
-    
-    
-}
-
-struct AddressView: View {
-    @ObservedObject var order: Order
-
-    var body: some View {
-        Text("Hello World")
-    }
-}
-
-struct AddressView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddressView(order: Order())
-    }
-}
-
 struct ContentView: View {
-    
-    
     
     @ObservedObject var order = Order()
 
---
+++ Cupcake Corner/Order.swift
@@ -0,0 +1,41 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Sebastien REMY on 18/06/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    @Published var type = 0
+    @Published var quantity = 3
+
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+
+        return true
+    }
+
+}
